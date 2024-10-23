6ebcf2bb6319cf79a057ea6c9501951ebebadfb8
mikle <mikle.kontinent@gmail.com>
2024-10-23 14:46:17 +0700

    initial commit

---
+++ L_1.2.2/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  L_1.2.2
+//
+//  Created by Таня Кожевникова on 23.10.2024.
+//
+
+import UIKit
+
+enum CurrentLight {
+    case red, yellow, green
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        print("Размер стороны доступной из viewDidLoad: ", redLight.frame.height)
+        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+        print("Размер стороны доступной из viewWillLayoutSubviews: ", redLight.frame.height)
+
+    }
+    
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+            
+        }
+    }
+}
+
